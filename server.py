b0fb42bead99407aaccfbc873b95b39c96f70166
Gabriel Esquivel <z5358503@ad.unsw.edu.au>
2025-03-04 22:44:03 -0700

    Create database on start up

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
-from app import app
+from app import app, db
+
+# Ensure tables are created before running the server
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     from gunicorn.app.wsgiapp import run
